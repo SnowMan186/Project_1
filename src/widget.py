f8737e51f4c938a6de478ade9e2cfb27fd2eb654
Eldar Isak <Isakzhanov186@bk.ru>
2025-08-17 12:35:37 +0500

    Initial commit: базовая структура проекта

--- src/widget.py
+++ src/widget.py
@@ -1,30 +1,21 @@
-from datetime import datetime
+from typing import Union
+
+def get_mask_card_number(card_number: int) -> str:
+    """Формирование маски банковской карты вида XXXX XX** **** XXXX. Args: card_number (int): Число, представляющее номер карты. Raises: ValueError: Если номер неверного формата. Returns: str: Маскированный номер карты."""
+    card_str = str(card_number)
+    if len(card_str) != 16 or not card_str.isdigit():
+        raise ValueError("Invalid card format.")
+    masked_part = '*' * 8
+    return f'{card_str[:4]} {card_str[4:6]} ** {masked_part} {card_str[-4]}'
+
+def get_mask_account(account_number: Union[int, str]) -> str:
+    """Формирование маски банковского счёта вида **XXXX. Args: account_number (Union[int, str]): Номер счёта. Raises: ValueError: Если счёт некорректного формата. Returns: str: Маскированный номер счёта."""
+    acc_str = str(account_number)
+    if len(acc_str) < 4 or not acc_str.isdigit():
+        raise ValueError("Invalid account format.")
+    return f'**{acc_str[-4]}'
 
 
-def mask_account_card(card_or_account_string):
-    """ Функция для маскирования номеров банковских карт и счетов. :param card_or_account_string: Строка вида 'Visa Platinum 7000792289606361' или 'Счет 73654108430135874305' :return: Маскированная строка вида '**** **** **** 6361' или '** ** *** ** ********* 4305' """
-    parts = card_or_account_string.split()
-    if len(parts) > 1 and parts[-1].isnumeric():
-        number = parts[-1]
-        prefix = ' '.join(parts[:-1])
-
-        # Если это карта, применяем стандартную маску карты
-        masked_number = f"{'*' * len(number[:-4])}{number[-4:]}"
-
-        # Если это счёт, используем специальную маску счёта
-        elif len(number) >= 16:
-        masked_number = f"{'*' * 4} {'*' * 4} {'*' * 3} {'*' * 2} {'*' * 6} {number[-4:]}"
-
-    return f"{prefix} {masked_number}"
-
-else:
-raise ValueError("Invalid input format")
-
-
-def get_date(iso_date_str):
-    """ Преобразует ISO-дату в удобный формат ДД.ММ.ГГГГ. :param iso_date_str: Дата в виде строки, например '2024-03-11T02:26:18.671407'. :return: Форматированная дата в виде '11.03.2024'. """
-    date_obj = datetime.fromisoformat(iso_date_str)
-    return date_obj.strftime("%d.%m.%Y")
 
 
 
