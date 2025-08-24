427416f827fe159ca7d42c7836eb0c73d801efde
Eldar Isak <Isakzhanov186@bk.ru>
2025-08-24 15:44:22 +0500

    homework 10_1 done

--- src/processing.py
+++ src/processing.py
@@ -1,19 +1,21 @@
+from typing import List, Dict
 from datetime import datetime
 
 
-def filter_by_state(transactions, state='EXECUTED'):
-    """ Фильтрует транзакции по состоянию. :param transactions: Список словарей с операциями :param state: Статус операций ('EXECUTED' по умолчанию) :return: Отфильтрованный список операций """
+def filter_by_state(transactions: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
+    """Фильтрует транзакции по заданному состоянию. Args: transactions (List[Dict]): Список словарей с операциями. state (str): Статус операций ('EXECUTED' по умолчанию). Returns: List[Dict]: Отфильтрованный список операций. """
     return [t for t in transactions if t.get('state') == state]
 
 
-def sort_by_date(transactions, ascending=False):
-    """ Сортирует операции по дате. :param transactions: Список словарей с операциями :param ascending: Если True, сортируем по возрастанию дат (по умолчанию False) :return: Отсортированный список операций """
+def sort_by_date(transactions: List[Dict], ascending: bool = False) -> List[Dict]:
+    """Сортирует операции по дате. Args: transactions (List[Dict]): Список словарей с операциями. ascending (bool): Если True, сортировка по возрастанию дат (по умолчанию False). Returns: List[Dict]: Отсортированный список операций. """
     sorted_transactions = sorted(
         transactions,
         key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%S.%f'),
         reverse=not ascending
     )
     return sorted_transactions
+
 
 transactions = [
     {'id': 1, 'state': 'EXECUTED', 'date': '2023-07-01'},
@@ -22,9 +24,7 @@ transactions = [
 ]
 
 filtered_transactions = filter_by_state(transactions)
-print(filtered_transactions)
-# Результат: [{...}, {...}] # Только выполненные транзакции
+print(filtered_transactions)  # Результат: [{...}, {...}] # Только выполненные транзакции
 
 sorted_transactions = sort_by_date(transactions)
-print(sorted_transactions)
-# Результат: сортируется по датам, начиная с последней
+print(sorted_transactions)     # Результат: сортируется по датам, начиная с последней
