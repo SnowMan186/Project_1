399bc2cfd0869db0e0577371243f12bfcd31027a
Eldar Isak <Isakzhanov186@bk.ru>
2025-08-30 15:35:56 +0500

    homework 10_1 done

--- src/masks.py
+++ src/masks.py
@@ -2,7 +2,7 @@ from typing import Union
 
 
 def get_mask_card_number(card_number: int) -> str:
-    """ Маскирует номер  карты формата ХХХХ ХХ** **** ХХХХ, оставляя видимым первые шесть и последние четыре цыфры. """
+    """ Маскирует номер  карты формата ХХХХ ХХ** **** ХХХХ, оставляя видимым первые шесть и последние четыре цифры. """
     card_str = str(card_number)
     if len(card_str) != 16 or not card_str.isdigit():
         raise ValueError("Неверный формат номера карты")
@@ -12,7 +12,7 @@ def get_mask_card_number(card_number: int) -> str:
 
 
 def get_mask_account(account_number: Union[int, str]) -> str:
-    """ Маскирует банковский счет формата **ХХХХ, отображая лишь последние четыре цыфры счета. """
+    """ Маскирует банковский счет формата **ХХХХ, отображая лишь последние четыре цифры счета. """
     acc_str = str(account_number)
     if len(acc_str) < 4 or not acc_str.isdigit():
         raise ValueError("Неверный формат номера счёта")
