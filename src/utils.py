79a465d9bb7ddd532af2e9364ca87883c1dc6e2f
Eldar Isak <Isakzhanov186@bk.ru>
2025-09-21 13:29:33 +0500

    Homework 12.2

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,27 @@
-import json
+import logging
+import os
+from pathlib import Path
 from typing import Union, List, Dict
+import json
+
+
+# Создание логера для модуля utils
+logger_utils = logging.getLogger('utils')
+logger_utils.setLevel(logging.DEBUG)
+
+# Определение пути к директории logs
+log_dir = Path(__file__).parent.parent / 'logs'
+os.makedirs(log_dir, exist_ok=True)
+
+# Форматтер сообщений лога
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Обработчик файла для log-потоков
+file_handler = logging.FileHandler(os.path.join(log_dir, 'utils.log'), mode='w')
+file_handler.setFormatter(formatter)
+
+# Добавляем хэндлер в логгер
+logger_utils.addHandler(file_handler)
 
 
 def read_json_file(file_path: str) -> Union[List[Dict], List]:
@@ -11,20 +33,22 @@ def read_json_file(file_path: str) -> Union[List[Dict], List]:
     :type file_path: str
     :rtype: list
     """
+    logger_utils.debug(f'Чтение файла {file_path}')
     try:
         with open(file_path, 'r', encoding='utf-8') as f:
             transactions = json.load(f)
 
-            # Проверяем, что данные представляют собой список
-            if isinstance(transactions, list):
-                return transactions
-            else:
-                print("Ошибка: Ожидается список транзакций")
-                return []
+        # Проверяем, что данные представляют собой список
+        if isinstance(transactions, list):
+            logger_utils.info(f'Файл успешно прочитан, количество транзакций: {len(transactions)}')
+            return transactions
+        else:
+            logger_utils.error('Ошибка формата данных: ожидается список транзакций.')
+            return []
 
     except FileNotFoundError:
-        print("Ошибка: Файл не найден")
+        logger_utils.error(f'Ошибка: файл "{file_path}" не найден.')
         return []
     except json.JSONDecodeError:
-        print("Ошибка: Некорректный формат JSON")
+        logger_utils.error(f'Ошибка: некорректный формат JSON в файле "{file_path}".')
         return []
